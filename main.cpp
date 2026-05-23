39c0b1bfb4ff20852daedd65195a52c8aeb3d0f4
Leonardo Kuhn <168349568+KuhnLeo@users.noreply.github.com>
2026-05-23 16:16:58 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,27 +3,9 @@
 #include <chrono>
 
 #include "listCreator.h"
+#include "sortings.h"
 
 using namespace std;
-
-void bubblesort(vector<int>& array) {
-    int n = array.size();
-    bool swapped;
-
-    for (int i = 0; i<n-1; i++) {
-        swapped = false;
-        for (int j = 0; j < n-i-1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(array[j], array[j+1]);
-                swapped = true;
-            }
-        }
-
-        if(!swapped) {
-            break;
-        }
-    }
-}
 
 int main() {
     int var;
