fd8ad02986a72f94fc3bcf5b3f2b9c54692e4b22
Leonardo Kuhn <168349568+KuhnLeo@users.noreply.github.com>
2026-05-23 16:10:37 -0300

    Create listCreator.h

---
+++ listCreator.h
@@ -0,0 +1,9 @@
+#ifndef LISTCREATOR_H
+#define LISTCREATOR_H
+
+vector<int> listaCrescente(int tamanho);
+vector<int> listaDecrescente(int tamanho);
+vector<int> listaAleatoriaSemRepeticao(int tamanho);
+vector<int> listaAleatoriaComRepeticao(int tamanho);
+
+#endif
