c926f4c7dfbff173a1e53d8290b46dea91b8936e
Leonardo Kuhn <168349568+KuhnLeo@users.noreply.github.com>
2026-05-23 16:16:22 -0300

    Create sortings.cpp

---
+++ sortings.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubblesort(vector<int>& array) {
+    int n = array.size();
+    bool swapped;
+
+    for (int i = 0; i<n-1; i++) {
+        swapped = false;
+        for (int j = 0; j < n-i-1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j+1]);
+                swapped = true;
+            }
+        }
+
+        if(!swapped) {
+            break;
+        }
+    }
+}
