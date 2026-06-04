09155346974b833416adbacb6be669778efe4054
Leonardo Kuhn <168349568+KuhnLeo@users.noreply.github.com>
2026-06-04 14:29:23 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,107 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <cmath>
+#include <iomanip>
 
 #include "listCreators/listCreator.h"
 #include "sortings/sortings.h"
+#include "output/calculadora.h"
 
 using namespace std;
 
+void executaSorting(int var, vector<int>& numeros){
+    if (var == 0) {
+        bubbleSort(numeros);
+    } else if (var == 1) {
+        insertionSort(numeros);
+    } else if (var == 2) {
+        selectionSort(numeros);
+    } else if (var == 3) {
+        heapSort(numeros);
+    } else if (var == 4) {
+        shellSort(numeros);
+    } else if (var == 5) {
+        mergeSort(numeros, 0, numeros.size() - 1);
+    } else if (var == 6) {
+        quickSort(numeros, 0, numeros.size() - 1);
+    }
+}
+
+string qualSorting(int var){
+    if (var == 0) {
+        return "Bubble Sort";
+    } else if (var == 1) {
+        return "Insertion Sort" ;
+    } else if (var == 2) {
+        return "Selection Sort";
+    } else if (var == 3) {
+        return "Heap Sort";
+    } else if (var == 4) {
+        return "Shell Sort";
+    } else if (var == 5) {
+        return "Merge Sort";
+    } else if (var == 6) {
+        return "Quick Sort";
+    }
+    return "Erro na escolha de sorting";
+}
+
 int main() {
-    int var = 10;
-    
-    vector<int> numeros = listaAleatoriaComRepeticao(var);
 
-    auto inicio = chrono::steady_clock::now();
+    cout << fixed << setprecision(2);
 
-    bubbleSort(numeros);
 
-    auto fim = chrono::steady_clock::now();
+    for (int i = 7; i <= 16; i++) {                                 // executa para 128 a 65536
 
-    auto duracao = fim - inicio;
-    auto duracaoNano = chrono::duration_cast<chrono::nanoseconds>(duracao);
+        int var = static_cast<int>(pow(2, i));                      // pow muda para double, com static cast fica int novamente
 
-    cout << "Tempo gasto para ordenar " << var << " números: " << duracaoNano.count() << " nanosegundos" << endl;
+        vector<int> numerosOriginal = listaAleatoriaComRepeticao(var);           // cria o array do tamanho da vez
+
+        for (int j = 0; j < 7; j++) {                                   // ajuda a determinar o sorting utilizado
+
+            vector<long long> tempos;
+
+            for (int k = 0; k < 10; k++) {                              // executa 10 vezes por sorting
+
+                vector<int> numeros = numerosOriginal;
+
+                auto inicio = chrono::steady_clock::now();
+
+                executaSorting(j, numeros);                         // define o sorting a ser executado
+
+                auto fim = chrono::steady_clock::now();
+
+                auto duracao = fim - inicio;
+                auto duracaoNano = chrono::duration_cast<chrono::nanoseconds>(duracao);
+
+                tempos.push_back(duracaoNano.count());
+
+            }
+            double media = valorMedia(tempos);
+            double variancia = valorVariancia(media, tempos);
+            double desvio = desvioPadrao(variancia);
+            double mediaFiltrada = mediaIntervalo(media, desvio, tempos);
+
+            cout << "Tamanho do array: "
+            << var << endl;
+            cout << "Sorting utilizado: "
+            << qualSorting(j) << endl;
+            cout << "Media de valor: "
+            << media
+            << endl;
+            cout << "Variância: "
+            << variancia
+            << endl;
+            cout << "Desvio padrão: "
+            << desvio
+            << endl;
+            cout << "Média filtrada: "
+            << mediaFiltrada
+            << endl << endl;
+            
+        }
+    }
     
     return 0;
 }
