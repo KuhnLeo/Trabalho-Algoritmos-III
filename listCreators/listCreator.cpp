57abe0f3295220391689bd1441fb49fddca132d5
Leonardo Kuhn <168349568+KuhnLeo@users.noreply.github.com>
2026-06-04 14:27:25 -0300

    Update listCreator.cpp

--- listCreators/listCreator.cpp
+++ listCreators/listCreator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <random>
+#include <algorithm>
 
 #include "listCreator.h"
 using namespace std;
@@ -22,16 +24,26 @@ vector<int> listaDecrescente(int tamanho) {
 }
 
 vector<int> listaAleatoriaSemRepeticao(int tamanho) {
-    vector<int> numeros(tamanho);
+    vector<int> numeros;
+    for (int i = 0; i < tamanho; i++) {
+        numeros.push_back(i);
+    }
+    random_device rd;
+    mt19937 g(rd());
+    shuffle(numeros.begin(), numeros.end(), g);
     
     return numeros;
 }
 
 vector<int> listaAleatoriaComRepeticao(int tamanho) {
-    vector<int> numeros(tamanho);
+    vector<int> numeros;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(0, tamanho - 1);
 
     for (int i = 0; i < tamanho; i++) {
-        numeros[i] = rand();
+        numeros.push_back(dist(gen));
     }
 
     return numeros;
